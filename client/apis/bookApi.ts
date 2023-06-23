470915994d5dd74465699b6ce695de5d74b12d64
James Idiens <jidiens@outlook.com>
2023-06-23 15:24:51 +1200

    tidied up api functions

--- client/apis/bookApi.ts
+++ client/apis/bookApi.ts
@@ -4,23 +4,13 @@ import { Book, NewBook } from '../../models/book'
 const rootUrl = '/api/v1'
 
 export async function fetchBooks() {
-  try {
-    const response = await request.get(`${rootUrl}/books`)
-    return response.body
-  } catch (error) {
-    console.error(error)
-    throw error
-  }
+  const response = await request.get(`${rootUrl}/books`)
+  return response.body
 }
 
 export async function createBook(newBook: NewBook): Promise<Book> {
-  try {
-    const response = await request.post(`${rootUrl}/books`).send(newBook)
-    return response.body as Book
-  } catch (error) {
-    console.error(error)
-    throw error
-  }
+  const response = await request.post(`${rootUrl}/books`).send(newBook)
+  return response.body as Book
 }
 
 export async function updateBook({
@@ -30,23 +20,11 @@ export async function updateBook({
   id: number
   updatedBook: Partial<Book>
 }): Promise<Book> {
-  try {
-    const response = await request
-      .put(`${rootUrl}/books/${id}`)
-      .send(updatedBook)
-    const updatedBookData: Book = response.body
-    return updatedBookData
-  } catch (error) {
-    console.error(error)
-    throw error
-  }
+  const response = await request.put(`${rootUrl}/books/${id}`).send(updatedBook)
+  const updatedBookData: Book = response.body
+  return updatedBookData
 }
 
 export async function deleteBook(id: number): Promise<void> {
-  try {
-    await request.delete(`${rootUrl}/books/${id}`)
-  } catch (error) {
-    console.error(error)
-    throw error
-  }
+  await request.delete(`${rootUrl}/books/${id}`)
 }
